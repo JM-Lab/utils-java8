d88cdcd9c73501b3b2d4ce585419d118f0ef04c4
허제민(Jemin Huh)/System Software 개발팀/SKP <jemin.huh@sk.com>
2016-07-15 14:29:01 +0900

    handle null or empty

--- src/main/java/kr/jm/utils/destory/Destroyer.java
+++ src/main/java/kr/jm/utils/destory/Destroyer.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import kr.jm.utils.exception.JMExceptionManager;
+import kr.jm.utils.helper.JMOptional;
 import kr.jm.utils.helper.JMThread;
 
 /**
@@ -25,7 +26,8 @@ public class Destroyer {
 	 */
 	public static <D extends DestroyInterface> void
 			cleanUp(@SuppressWarnings("unchecked") D... destorys) {
-		cleanUp(Arrays.asList(destorys));
+		JMOptional.getOptional(destorys).map(Arrays::asList)
+				.ifPresent(Destroyer::cleanUp);
 	}
 
 	/**
@@ -38,17 +40,26 @@ public class Destroyer {
 	 */
 	public static <D extends DestroyInterface> void
 			cleanUp(List<D> destroyList) {
-		for (DestroyInterface destroy : destroyList) {
-			try {
-				JMThread.sleep(10);
-				log.info("Start Clean Up - {}", destroy);
-				destroy.cleanUp();
-				log.info("Complete Clean Up - {}", destroy);
-			} catch (Exception e) {
-				log.error("Fail Clean Up - {}", destroy);
-				JMExceptionManager.logException(log, e, "cleanUp", destroy);
-				continue;
-			}
+		JMOptional.getOptional(destroyList)
+				.ifPresent(list -> list.forEach(Destroyer::cleanUp));
+	}
+
+	/**
+	 * Clean up.
+	 *
+	 * @param destroy
+	 *            the destroy
+	 */
+	public static void cleanUp(DestroyInterface destroy) {
+		try {
+			JMThread.sleep(10);
+			log.info("Start Clean Up - {}", destroy);
+			destroy.cleanUp();
+			log.info("Complete Clean Up - {}", destroy);
+		} catch (Exception e) {
+			log.error("Fail Clean Up - {}", destroy);
+			JMExceptionManager.logException(log, e, "cleanUp", destroy);
+			return;
 		}
 	}
 
